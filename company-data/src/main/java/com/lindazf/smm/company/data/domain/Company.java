db6c4084a317344e0e85eff2129843dab16cc1e2
Linda Zou <49109592+lindafz@users.noreply.github.com>
2021-06-17 21:35:00 -0700

    Update Company.java

--- company-data/src/main/java/com/lindazf/smm/company/data/domain/Company.java
+++ company-data/src/main/java/com/lindazf/smm/company/data/domain/Company.java
@@ -1,17 +1,17 @@
 package com.lindazf.smm.company.data.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "company")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Company implements Serializable
 {
     @Id
@@ -24,8 +24,4 @@ public class Company implements Serializable
 
     @Column(name = "address")
     private String address;
-
-    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<CompanyUser> companyMembers = new HashSet<>();
-
 }
